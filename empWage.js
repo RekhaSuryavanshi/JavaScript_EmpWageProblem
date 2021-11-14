6f32543acc98af16feb5ad17a775e1c5329b3956
RekhaSuryavanshi <rrsuryawanshi24@gmail.com>
2021-11-13 22:08:03 -0800

    solved empWage problem using if statement

--- empWage.js
+++ empWage.js
@@ -1,5 +1,4 @@
 /*Ability to check employee is present or absent*/
-console.log("Welcome to EmployeeWage");
 const IS_ABSENT = 0;
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck != IS_ABSENT) {
